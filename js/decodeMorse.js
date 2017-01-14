c4ee4919f11055952a56bac4ef2ff85f8d32e04f
zhongyi <zhongyi.qiu@xiaobao100.com>
2017-01-14 13:16:04 +0800

    未过滤不正常组合

--- js/decodeMorse.js
+++ js/decodeMorse.js
@@ -1,31 +1,31 @@
-
-
-var EC=function(data){
+var EC = function(data) {
 	//EC:EscapeCharacter
-	return data.replace(/\./g,'\\\.');
+	return data.replace(/\./g, '\\\.');
 }
+//结果数组
+var res = [];
 
-var res=[];
-var DecodeMorse=function(model){
+//按钮绑定事件
+var DecodeMorse = function(model) {
 	$("#processedText").val('');
 	$("#wordCount").html(0);
 
-	res=[];
-	var sourceText=$("#sourceText").val();
-	sourceText=sourceText.trim();
+	res = [];
+	var sourceText = $("#sourceText").val();
+	sourceText = sourceText.trim();
 
-	var res1 = changeChart(sourceText); 
-	if(!res1){
+	var res1 = changeChart(sourceText);
+	if (!res1) {
 		$("#processedText").val("请确保只输入了两种字符");
 		return;
 	}
-	var c1=res1.chart1;
-	var c2=res1.chart2;
-	res1=res1.sourceText;
+	var c1 = res1.chart1;
+	var c2 = res1.chart2;
+	res1 = res1.sourceText;
 
 	Decode(res1);
 
-	if(model==1){//过滤部分字符
+	if (model == 1) { //过滤部分字符
 		res = wordFilter(res);
 	}
 
@@ -34,18 +34,19 @@ var DecodeMorse=function(model){
 	$("#processedText").val(res.join('\n'));
 }
 
-var wordFilter = function(data){
-	data = data.filter(function(item,index){
+//试图过滤部分不正常的结果
+var wordFilter = function(data) {
+	data = data.filter(function(item, index) {
 		var pa = new RegExp('(\.)(\\1){2,}');
-		if(index==1){
+		if (index == 1) {
 			//console.log(pa)
 		}
-		if(!pa.test(item)){
+		if (!pa.test(item)) {
 			return true;
-		}else{
+		} else {
 			console.log(item);
 		}
-	});//过滤掉有三个+重复字母的
+	}); //过滤掉有三个+重复字母的
 
 	return data;
 
@@ -66,83 +67,89 @@ var wordFilter = function(data){
 	// $.ajax(ajax);
 }
 
+//对输入字符进行处理
+var changeChart = function(data) {
+	var d1 = data.trim();
+	d1 = d1.replace(/\n|\s/g, '');
 
-var changeChart = function(data){
-	var d1=data.trim();
-	d1=d1.replace(/\n|\s/g,'');
-
-	 var d2=[];
-	for(var i=0;i<d1.length;i++){
+	var d2 = [];
+	for (var i = 0; i < d1.length; i++) {
 		d2.push(d1[i]);
 	}
-	var c1=d2[0];
-	var c2='';
-	for(var i=0;i<d2.length;i++){
-		if(d2[i] != c1){
-			c2=d2[i];
+	var c1 = d2[0];
+	var c2 = '';
+	for (var i = 0; i < d2.length; i++) {
+		if (d2[i] != c1) {
+			c2 = d2[i];
 			break;
 		}
 	}
 
-	var sc1='.';
-	var sc2='-'
+	var sc1 = '.';
+	var sc2 = '-'
 
-	var isFalse=false;
-	d2.forEach(function(item,index,array){
-		if(item==c1){
-			array[index]=sc1;
-		}else if(item==c2){
-			array[index]=sc2;
-		}else{
-			isFalse=true;
+	var isFalse = false;
+	d2.forEach(function(item, index, array) {
+		if (item == c1) {
+			array[index] = sc1;
+		} else if (item == c2) {
+			array[index] = sc2;
+		} else {
+			isFalse = true;
 		}
 	});
 
-	if(isFalse){
+	if (isFalse) {
 		return false;
 	}
-	d2=d2.join('');
+	d2 = d2.join('');
 
-	return {sourceText:d2,chart1:c1,chart2:c2};
+	return {
+		sourceText: d2,
+		chart1: c1,
+		chart2: c2
+	};
 	console.log(d2);
 	// var t=new RegExp();
 }
 
-
-var Decode = function(data,data1){
+//递归算法
+var Decode = function(data, data1) {
 	//var remain=data
-	if(data1 == undefined){
-		data1='';
+	if (data1 == undefined) {
+		data1 = '';
 	}
-	for(var i=0;i<codeList.length;i++){
+	for (var i = 0; i < codeList.length; i++) {
 
-		var remain=data;
-		var preRes=data1;
+		var remain = data;
+		var preRes = data1;
 
-	    var testCode = '^'+EC(codeList[i].mCode);//尝试码
-	    testCode = new RegExp(testCode);
-		var	pData = remain.replace(testCode,'');
+		var testCode = '^' + EC(codeList[i].mCode); //尝试码
+		testCode = new RegExp(testCode);
+		var pData = remain.replace(testCode, '');
 
-		if(pData && pData != remain){//如果存在且未结束
-			preRes+=codeList[i].letter;
+		if (pData && pData != remain) { //如果存在且未结束
+			preRes += codeList[i].letter;
 
-			Decode(pData,preRes);
+			Decode(pData, preRes);
 		}
-		if(!pData){//完美结束
-			res.push(preRes+codeList[i].letter);
+		if (!pData) { //完美结束
+			res.push(preRes + codeList[i].letter);
 			//return true;
 		}
 	}
 }
 
-$(function(){
-	$(document).keydown(function(event){ 
-		if(event.keyCode==13){ 
+//绑定回车键
+$(function() {
+	$(document).keydown(function(event) {
+		if (event.keyCode == 13) {
 			DecodeMorse();
 		}
 	});
 });
 
+//摩斯码枚举
 var codeList=[
 		{letter:'a',mCode:'.-'},
 		{letter:'b',mCode:'-...'},
